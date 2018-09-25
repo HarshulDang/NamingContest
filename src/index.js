6b01b0dd473f2019b140f74e2c03545584259115
HarshulDang <dangharshul199430@gmail.com>
2018-09-25 22:32:32 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({ message }) => {
+	return(
+		<h2 className="text-center">
+			{message}
+		</h2>
+		
+	);
+};
+
+Header.propTypes = {
+	message: React.PropTypes.string
+};
+
+const App = () => {
+	return(
+		<div>
+			<Header message="Naming Contests"/>
+			<div>
+			   ...
+			</div>
+		</div>	
+	);
+};
+
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
